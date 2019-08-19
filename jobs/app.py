95c8884badcfb2789607002e8eaba597af5b10eb
TA <51969618+ta@users.noreply.github.com>
2019-08-19 11:00:05 -0500

    module 7

--- jobs/app.py
+++ jobs/app.py
@@ -10,7 +10,7 @@ def employer(employer_id):
     employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
-    return render_template("employer.html", employer = employer, jobs = jobs, reviews = reviews)
+    return render_template("employer.html", employer=employer, jobs=jobs, reviews=reviews)
 
 @app.route("/employer/<employer_id>/review", methods=['GET', 'POST'])
 def review(employer_id):
@@ -20,8 +20,8 @@ def review(employer_id):
         title = request.form['title']
         status = request.form['status']
         date = datetime.datetime.now().strftime("%m/%d/%Y")
-        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)' , (review, rating, title, date, status, employer_id), commit=True)
-        return redirect("url_for('employer', employer_id=employer_id)")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+        return redirect(url_for('employer', employer_id=employer_id))
     return render_template("review.html", employer_id=employer_id)
 
 @app.route("/job/<job_id>")
